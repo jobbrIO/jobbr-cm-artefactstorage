3f1ad3235551585b74cde64ae255688b745168a5
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-01-28 16:14:29 +0100

    removed FileInfo from IArtefactStorageProvider interface since its coupled to the filesystem. replaced by JobbrArtefact

---
+++ source/Jobbr.ComponentModel.ArtefactStorage/JobbrArtefact.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace Jobbr.ComponentModel.ArtefactStorage
+{
+    public class JobbrArtefact
+    {
+        public string FileName { get; set; }
+
+        public Stream Data { get; set; }
+    }
+}
