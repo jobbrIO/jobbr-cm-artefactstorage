3dd093df3f4164c6b7c744bc195a2da0a5c334c9
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-03-27 19:44:47 +0200

    cleanup & added docs

--- source/Jobbr.ComponentModel.ArtefactStorage/IArtefactsStorageProvider.cs
+++ source/Jobbr.ComponentModel.ArtefactStorage/IArtefactsStorageProvider.cs
@@ -6,10 +6,27 @@ namespace Jobbr.ComponentModel.ArtefactStorage
 {
     public interface IArtefactsStorageProvider
     {
+        /// <summary>
+        /// Stores a file uniquely identifyable by the container and the filename
+        /// </summary>
+        /// <param name="container">Usually relates to a JobRun and contains one or multiple files</param>
+        /// <param name="fileName">The original filenName that was used when creating the file</param>
+        /// <param name="content">Stream to access the contents of the file</param>
         void Save(string container, string fileName, Stream content);
 
+        /// <summary>
+        /// Access a file's contents by providing the container and fileName as unique keys
+        /// </summary>
+        /// <param name="container">Usually relates to a JobRun and contains one or multiple files</param>
+        /// <param name="fileName">The original filenName that was used when creating the file</param>
+        /// <returns>A stream the gives access to the file-contents</returns>
         Stream Load(string container, string fileName);
 
+        /// <summary>
+        /// Provides access to all artefacts/files in a given container
+        /// </summary>
+        /// <param name="container">Usually relates to a JobRun and contains one or multiple files</param>
+        /// <returns>A list of <see cref="JobbrArtefact"/>s</returns>
         List<JobbrArtefact> GetArtefacts(string container);
     }
 }
--- source/Jobbr.ComponentModel.ArtefactStorage/Model/JobbrArtefact.cs
+++ source/Jobbr.ComponentModel.ArtefactStorage/Model/JobbrArtefact.cs
@@ -1,13 +1,24 @@
-﻿using System.IO;
-
-namespace Jobbr.ComponentModel.ArtefactStorage.Model
+﻿namespace Jobbr.ComponentModel.ArtefactStorage.Model
 {
+    /// <summary>
+    /// A JobbrArtefact encapsulates one file that has been generated while the job was run.
+    /// It's the responsibility of the provider to store this metainformation along the file contents
+    /// </summary>
     public class JobbrArtefact
     {
+        /// <summary>
+        /// Gets or sets the original fileName of the file when it was created by the job
+        /// </summary>
         public string FileName { get; set; }
 
+        /// <summary>
+        /// Gets or setzs the MimeType of the created file
+        /// </summary>
         public string MimeType { get; set; }
         
+        /// <summary>
+        /// Gets or sets the size of the file in bytes
+        /// </summary>
         public long Size { get; set; }
     }
 }
