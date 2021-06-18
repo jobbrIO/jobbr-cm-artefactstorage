09a88f8db5c48a54e5aa64af9f403bb7f0654209
Steven Giesel <stgiesel35@gmail.com>
2021-06-18 08:07:09 +0200

    Updated to .netstandard2.0

--- source/Jobbr.ComponentModel.ArtefactStorage/IArtefactsStorageProvider.cs
+++ source/Jobbr.ComponentModel.ArtefactStorage/IArtefactsStorageProvider.cs
@@ -10,7 +10,7 @@ namespace Jobbr.ComponentModel.ArtefactStorage
         /// Stores a file uniquely identifyable by the container and the filename
         /// </summary>
         /// <param name="container">Usually relates to a JobRun and contains one or multiple files</param>
-        /// <param name="fileName">The original filenName that was used when creating the file</param>
+        /// <param name="fileName">The original fileName that was used when creating the file</param>
         /// <param name="content">Stream to access the contents of the file</param>
         void Save(string container, string fileName, Stream content);
 
@@ -18,7 +18,7 @@ namespace Jobbr.ComponentModel.ArtefactStorage
         /// Access a file's contents by providing the container and fileName as unique keys
         /// </summary>
         /// <param name="container">Usually relates to a JobRun and contains one or multiple files</param>
-        /// <param name="fileName">The original filenName that was used when creating the file</param>
+        /// <param name="fileName">The original fileName that was used when creating the file</param>
         /// <returns>A stream the gives access to the file-contents</returns>
         Stream Load(string container, string fileName);
 
